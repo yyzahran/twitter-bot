10dfd8021511219a045c8927c8d33187e5826e59
Youssef Zahran <55523423+yyzahran@users.noreply.github.com>
2021-02-11 22:22:52 -0700

    more lyrics and now I made 2 bots doing the same function

---
+++ TwitterBot_scheuled.py
@@ -0,0 +1,46 @@
+import tweepy
+import time
+import schedule
+import random
+
+# logging in
+consumer_key = "xCkC9VMdgmRtyLWpz1FGMgKH5" # API KEY
+consumer_secret = "8RSVwnnG06zApTKQVyxOIJqMEmgXjUxiTidrq5h3aVdEyvihlZ" # API SECRET KEY
+bearer_key = """AAAAAAAAAAAAAAAAAAAAALxGLgEAAAAAOk4nvPLf4%2F%2BwspCiaDGlKyF1YbA%3Dt33TdvGQfjMhmUZo3N7peBipBkJhfivduhWBEDbqP5r6dSC3Kb""" # BEARER KEY
+access_token = "1348505272780607489-8GNvEZZVCkRQT76nNdk3Pn75UuYr6I"
+access_token_secret = "XwEDZl0o45y8RKVxTqMD9TG3HLJ0X6Y8ubrTe1HZghlwN"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# open the lyrics file and generate a random line
+def extract_lyric_line():
+    with open('radioheadLyrics.txt', 'r') as lyrics_file:
+        l = []
+        for lines in lyrics_file.readlines():
+            if not lines.strip():
+                continue
+            if lines.isalpha:
+                l.append(lines.rstrip())
+        return random.choice(l)
+
+
+# function to post a tweet everyday
+def post_tweet():
+    try:
+        api.update_status(extract_lyric_line())
+    except tweepy.TweepError as error:
+        while error.api_code == 187:
+            print('Duplicate tweet!', extract_lyric_line())
+        else:
+            raise error
+
+
+# timing
+schedule.every(1).day.at('11:00').do(post_tweet)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
